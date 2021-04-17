1d43b37887443fa2f35e0fb67c2141692ff3cfcb
My Tran Bui <mytranbui@Mys-MacBook-Pro.local>
2021-04-17 14:00:37 +0200

    pushed

--- 42Filler/src/utils2.c
+++ 42Filler/src/utils2.c
@@ -6,7 +6,7 @@
 /*   By: mbui <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 18:11:25 by mbui              #+#    #+#             */
-/*   Updated: 2021/04/08 18:18:46 by mbui             ###   ########.fr       */
+/*   Updated: 2021/04/17 14:00:30 by mbui             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ int	touch_border_left(t_filler *f)
 
 int	exit_n_free(t_filler *f)
 {
+	ft_printf("0 0\n");
 	free_tabs(f);
 	exit(1);
 }
